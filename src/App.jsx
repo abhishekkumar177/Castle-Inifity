e34cbe408adc3dd1618e9556d59e2c645b0af96e
abhishekkumar177 <abhishekumar1772004@gmail.com>
2025-09-21 23:54:33 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Castle1 from './components/Castle1.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-black">
+      <Castle1 />
+    </div>
+  );
+}
+
+export default App;
